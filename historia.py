71212b03d5574cb6ed8291542e62ad2ff3032ac2
heloisamariasl <heloisamsleite@gmail.com>
2025-03-18 19:31:34 -0300

    Update and rename historia.py to helo.historia.py

--- historia.py
+++
@@ -1,9 +0,0 @@
-nome = "Gávic"
-
-idade = 19
-
-local = "Esgoto"
-
-poder = "Elemental"
-
-print(f"O assassino {nome} de {idade} anos, que mora no {local}, tem um grande poder {poder}, com capaciade de distruir reinos e mundos.")
