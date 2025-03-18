048cca76e9c935422deca939061105c1c6e57512
heloisamariasl <heloisamsleite@gmail.com>
2025-03-18 14:28:35 -0300

    gabriel.helo.historia.py

--- historia.py
+++ historia.py
@@ -1,9 +1,15 @@
 nome = "Gávic"
 
+nome2 = "Hello"
+
 idade = 19
+
+idade2= 16
 
 local = "Esgoto"
 
 poder = "Elemental"
 
-print(f"O assassino {nome} de {idade} anos, que mora no {local}, tem um grande poder {poder}, com capaciade de distruir reinos e mundos.")
+poder2= "invisibilidade"
+
+print(f"O assassino {nome} de {idade} anos, que mora no {local}, tem um grande poder {poder}, com capaciade de distruir reinos e mundos. Mas surge uma grande lutadora {nome2}, de {idade2} idade, com o super poder de {poder2}, que está preparada para combate-ló")
